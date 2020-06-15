3d9641f671ba484570130bd1fac9b91dbb742d65
Martin Achtermann <mdacht777@gmail.com>
2020-06-15 04:02:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,9 +7,9 @@ class School
   end
   def add_student(name,grade)
     if @roster[grade]
-      @roster[grade]=[name]
-    else
       @roster[grade]<<name
+    else
+      @roster[grade]=[name]
     end
   end
 end
