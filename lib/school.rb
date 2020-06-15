de9164c5308f772d0b4cb9cb50573ee3f9272963
Martin Achtermann <mdacht777@gmail.com>
2020-06-15 03:47:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,11 @@
 # code here!
 class School
   attr_reader :name, :roster
-  ROSTER={}
   def initialize(name)
     @name=name
     @roster={}
   end
   def add_student(arg)  
-    @roster >> arg
+    @roster << arg
   end
 end
